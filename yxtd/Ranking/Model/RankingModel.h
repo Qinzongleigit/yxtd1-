3a904633db48a14373b0312e6342fd77b803eb16
覃宗雷 <qinzonglei@tanzongleideiMac.local>
2017-11-27 00:29:23 +0800

    添加了排行model

---
+++ yxtd/Ranking/Model/RankingModel.h
@@ -0,0 +1,13 @@
+//
+//  RankingModel.h
+//  yxtd
+//
+//  Created by 覃宗雷 on 2017/11/27.
+//  Copyright © 2017年 qin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RankingModel : NSObject
+
+@end
---
+++ yxtd/Ranking/Model/RankingModel.m
@@ -0,0 +1,13 @@
+//
+//  RankingModel.m
+//  yxtd
+//
+//  Created by 覃宗雷 on 2017/11/27.
+//  Copyright © 2017年 qin. All rights reserved.
+//
+
+#import "RankingModel.h"
+
+@implementation RankingModel
+
+@end
